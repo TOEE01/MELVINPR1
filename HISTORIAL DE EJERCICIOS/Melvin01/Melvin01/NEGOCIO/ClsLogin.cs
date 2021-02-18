0061e633b500e71b001be183fe9510ad249aa02d
Melvin Borja <rb0587032020@unab.edu.sv>
2021-02-18 15:08:11 -0600

    CARPETA DE HISTORIAL

---
+++ HISTORIAL DE EJERCICIOS/Melvin01/Melvin01/NEGOCIO/ClsLogin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Melvin01.DOMINIO;
+
+namespace Melvin01.NEGOCIO
+{
+    class ClsLogin
+    {
+        Login log = new Login();
+    }
+}
